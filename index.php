cc81eef537c65c8a9141b0460b7c0540077e4a92
Swondir <hhghghf@mail.ua>
2017-02-10 21:52:53 +0200

    Added main menu without functions(10.02.17)

---
+++ index.php
@@ -0,0 +1,32 @@
+<DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>English</title>
+<link href="css\style.css" rel="stylesheet">
+<meta name='viewport' content='width=device-width, initial-scale=1.0'/>
+ </head>
+  <body>
+  	<div id="page">
+  		<header>
+	  		<div class="date-box">
+	  		</div>
+	  		<div class="logo-overlay">
+	  			<div class="logo">
+
+	  			</div>
+	  		</div>
+	  		<div class="site-navigation">
+	  			<ul>
+	  				<li><a href="#">Пункт 1</a></li>
+	  				<li><a href="#">Пункт 2</a></li>
+	  				<li><a href="#">Пункт 3</a></li>
+	  				<li><a href="#">Пункт 4</a></li>
+	  				<li><a href="#">Пункт 5</a></li>
+	  			</ul>
+	  		</div>
+  		</header>
+  	</div>
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/2.2.0/jquery.min.js"></script>
+ </body>
+</html>
